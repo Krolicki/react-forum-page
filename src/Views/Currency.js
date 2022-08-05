1e9084cda40743477d4ef9961d7dbc14f6c2a4c0
unknown <jakub.krolicki.k@gmail.com>
2022-08-05 22:46:13 +0200

    fetch, error, loading

--- src/Views/Currency.js
+++ src/Views/Currency.js
@@ -1,12 +1,25 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { Loading } from "../Components"
 
 export const Currency = () => {
     const {code} = useParams()
     const url = `http://api.nbp.pl/api/exchangerates/rates/a/${code}`
-    const [rate, setRate] = useState(null)
+    const [rate, setRate] = useState({
+        loading: false,
+        data: null,
+        error: false
+    })
+    const [errorMessage, setErrorMessage] = useState(null)
     
+    let content = null
+
     useEffect(()=>{
+        setRate({
+            loading: true,
+            data: null,
+            error: false
+        })
         fetch(url)
             .then((response) => {
                 if(response.ok)
@@ -14,25 +27,43 @@ export const Currency = () => {
                 throw response
             })
             .then(response => response.json())
-            .then(response => setRate(response))
-            .catch(()=>{
-                console.log("nie działa")
+            .then(response => setRate({
+                loading: false,
+                data: response,
+                error: false
+            }))
+            .catch(err=>{
+                setRate({
+                    loading: false,
+                    data: null,
+                    error: true
+                })
+                setErrorMessage(`Status: ${err.status}`)
             })
     },[url])
 
-    if(rate){
-        return(
-            <div>
-                <h1>Kurs {code}</h1>
-                <p>1 PLN = {rate.rates[0].mid}</p>
-                <img src="https://unsplash.it/920/600" alt="some picture"/>
-            </div>
-        )
+    if(rate.error){
+        content = <div>
+            <p>Błąd podczas pobierania danych</p>
+            <p>{errorMessage}</p>
+        </div>
+    }
+
+    if(rate.loading){
+        content= <Loading />
+    }
+
+    if(rate.data){
+        content = <div>
+            <h1>Kurs {code}</h1>
+            <p>1 PLN = {rate.data.rates[0].mid}</p>
+            <img src="https://unsplash.it/920/600" alt="some picture"/>
+        </div>
     }
 
     return(
         <div>
-            <p>Wczytywanie...</p>
+            {content}
         </div>
     )
 }
