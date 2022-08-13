2b30d57835ba4cd18e8e8b9201f16c4b38337200
unknown <jakub.krolicki.k@gmail.com>
2022-08-13 16:24:24 +0200

    Landing Page - contact

--- src/Apps/Landing-Page/components/About.js
+++ src/Apps/Landing-Page/components/About.js
@@ -1,9 +1,11 @@
 import './About.css'
+import image from '../assets/3.jpg'
 
 export const About = () =>{
     return (
         <div className='container'>
             <p>about</p>
+            <img src={image} alt="image" />
         </div>
     )
 }
