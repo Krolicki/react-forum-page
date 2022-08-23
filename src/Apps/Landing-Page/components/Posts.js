88907fc9967fba221a5cbe7e5f0bd89f847cfcaf
unknown <jakub.krolicki.k@gmail.com>
2022-08-23 17:32:45 +0200

    Landing Page - get posts

--- src/Apps/Landing-Page/components/Posts.js
+++ src/Apps/Landing-Page/components/Posts.js
@@ -1,6 +1,38 @@
+import { useEffect, useState } from 'react'
 import './styles/Posts.css'
 
 export const Posts = () => {
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [currentPage, setCurentPage] = useState(1)
+    const [postsPerPage, setPostPerPage] = useState(10)
+
+    useEffect(()=>{
+        const getPosts = async () => {
+            setLoading(true)
+            const postsList = await fetch("http://localhost:5000/posts")
+            .then(respsonse =>{
+                if(respsonse.ok)
+                    return respsonse.json()
+                throw respsonse
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+            setPosts(postsList)
+            setLoading(false)
+        }
+        getPosts()
+    }, [])
+
+    if(loading){
+        return(
+            <div className='posts-container'>
+                <h1>Loading posts...</h1>
+            </div>
+        )
+    }
+
     return (
         <div className='posts-container'>
             <section className='posts'>
