13a84123fb4e5bb3715aa41d187157fabe8fd2cc
unknown <jakub.krolicki.k@gmail.com>
2022-08-31 13:23:56 +0200

    Landing Page - delete post

--- src/Apps/Landing-Page/components/Post.js
+++ src/Apps/Landing-Page/components/Post.js
@@ -7,8 +7,10 @@ export const Post = () => {
     const {id} = useParams()
     const [post, setPost] = useState([])
     const [loading, setLoading] = useState(false)
+    const [postNotFound, setPostNotFound] = useState(false)
     const [showDeleteWindow, setShowDeleteWindow] = useState(false)
     const [animateDeleteWindow, setAnimateDeleteWindow] = useState(false)
+    const [postDeleted, setPostDeleted] = useState(false)
 
     useEffect(() => {
         const getPost = async () => {
@@ -22,11 +24,30 @@ export const Post = () => {
                 .catch((err) => {
                     console.log(err)
                 })
-            setPost(postFromAPI[0])
+            if(postFromAPI.length !== 0)
+                setPost(postFromAPI[0])
+            else
+                setPostNotFound(true)
             setLoading(false)
         }
         getPost()
     }, [])
+
+    const deletePost = async () => {
+        await fetch(`http://localhost:5000/posts/${post.id}`, {
+            method: 'DELETE',
+        })
+        .then(respsonse => {
+            if(respsonse.ok){
+                setPostDeleted(true)
+                return respsonse
+            }
+            throw respsonse
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     const showHideDeleteWindow = (showHide) => {
         if(showHide){
@@ -48,6 +69,21 @@ export const Post = () => {
             <div className='posts-container'>
                 <h1>Loading posts...</h1>
             </div>
+        )
+    }
+
+    if (postNotFound) {
+        return (
+            <section className='post-container'>
+                <div className="delete-window show-delete-window">
+                    <div className="delete-window-content">
+                        <h2>Post not found</h2>
+                        <Link to={`/posts`} className="delete-window-buttons">
+                            <button type="button">Back to Posts</button>
+                        </Link>
+                    </div>
+                </div>
+            </section>
         )
     }
     return(
@@ -75,11 +111,22 @@ export const Post = () => {
             {showDeleteWindow && 
                 <div className={`delete-window ${animateDeleteWindow ? "show-delete-window" : ""}`}>
                     <div className="delete-window-content">
-                        <h2>Are you sure you want to delete post "{post.title}?"</h2>
-                        <span className="delete-window-buttons">
-                            <button>Yes</button>
-                            <button onClick={()=>showHideDeleteWindow(false)}>Cancel</button>
-                        </span>
+                        {!postDeleted ?
+                        <>
+                            <h2>Are you sure you want to delete post "{post.title}"?</h2>
+                            <span className="delete-window-buttons">
+                                <button onClick={()=>deletePost()}>Yes</button>
+                                <button onClick={()=>showHideDeleteWindow(false)}>Cancel</button>
+                            </span>
+                        </>
+                        :
+                        <> 
+                            <h2>Post "{post.title}" has been deleted</h2>
+                            <Link to={`/posts`} className="delete-window-buttons">
+                                <button type="button">Back to Posts</button>
+                            </Link>
+                        </>
+                        }
                     </div>
                 </div>
             }
