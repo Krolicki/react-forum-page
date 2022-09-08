6ebeeab01520126f277b6c5ed5002095422676d6
unknown <jakub.krolicki.k@gmail.com>
2022-09-08 21:33:22 +0200

    Landing Page - most viewed posts

--- src/Apps/Landing-Page/components/Posts.js
+++ src/Apps/Landing-Page/components/Posts.js
@@ -21,8 +21,9 @@ export const Posts = () => {
                 .catch((err) => {
                     console.log(err)
                 })
+            getMostViewed(postsList)
             setPosts(postsList.reverse())
-            setLoading(false)
+            setLoading(false) 
         }
         getPosts()
     }, [])
@@ -30,6 +31,43 @@ export const Posts = () => {
     const indexLastPost = currentPage * postsPerPage
     const indexFirstPost = indexLastPost - postsPerPage
     const currentPosts = posts.slice(indexFirstPost, indexLastPost)
+
+    const paginate = (num) => setCurentPage(num)
+
+    const [mostViewed, setMostViewed] = useState([])
+ 
+    const getMostViewed = (list) => {
+        let first, second, third
+        let firstID, secondID, thirdID
+        first = second = third = Number.MIN_VALUE
+
+        list.forEach(post => {
+            if(post.views !==undefined){
+                if(post.views > first){
+                    third = second;
+                    second = first;
+                    first = post.views;
+                    thirdID = secondID;
+                    secondID = firstID;
+                    firstID = post;
+                }
+                else if (post.views > second)
+                {
+                    third = second;
+                    second = post.views;
+                    thirdID = secondID;
+                    secondID = post;
+                }
+                else if (post.views > third){
+                    third = post.views;
+                    thirdID = post;
+                }
+            }
+        })
+        console.log(firstID, secondID, thirdID)
+        setMostViewed([firstID, secondID, thirdID])
+        console.log(mostViewed)
+    }
 
     if (loading) {
         return (
@@ -39,49 +77,76 @@ export const Posts = () => {
         )
     }
 
-    const paginate = (num) => setCurentPage(num)
-
     return (
-        <div className='posts-container' id="posts-top">
-            <Pagination
-                postsPerPage={postsPerPage} 
-                totalPosts={posts.length} 
-                paginate={paginate}
-                currentPage={currentPage}
-            />
-            <Link to="/newpost" className='new-post-button'><button>New Post</button></Link>
-            <section className='posts'>
-                {currentPosts.map(post => {
-                    return (
-                        <div className='post' key={post.id}>
-                            <div className='post-head'>
-                                <div className='post-title'>
-                                    <h2>{post.title}</h2>
-                                    {post.edit?
-                                        <p>Edited: {post.edit}</p>
-                                    :
-                                        <p>Posted: {post.date}</p>
-                                    }   
+        <div className='posts-wraper' id="posts-top">
+            <section className='posts-container'>
+                <Pagination
+                    postsPerPage={postsPerPage} 
+                    totalPosts={posts.length} 
+                    paginate={paginate}
+                    currentPage={currentPage}
+                />
+                <Link to="/newpost" className='new-post-button'><button>New Post</button></Link>
+                <section className='posts'>
+                    {currentPosts.map(post => {
+                        return (
+                            <div className='post' key={post.id}>
+                                <div className='post-head'>
+                                    <div className='post-title'>
+                                        <h2>{post.title}</h2>
+                                        {post.edit?
+                                            <p>Edited: {post.edit}</p>
+                                        :
+                                            <p>Posted: {post.date}</p>
+                                        }   
+                                    </div>
                                 </div>
+                                <p className='post-desc'>{post.desc}</p>
+                                <div className='post-info'>
+                                    {post.user !==undefined ? <p>Posted by: {post.user}</p> : <></>}
+                                    <p>Views: {post.views !==undefined ? post.views : "0"}</p>
+                                </div>
+                                <Link to={`/post/${post.id}`}>
+                                    <button type='button'>Show post</button>
+                                </Link>
                             </div>
-                            <p className='post-desc'>{post.desc}</p>
-                            <div className='post-info'>
-                                {post.user !==undefined ? <p>Posted by: {post.user}</p> : <></>}
-                                <p>Views: {post.views !==undefined ? post.views : "0"}</p>
-                            </div>
-                            <Link to={`/post/${post.id}`}>
-                                <button type='button'>Show post</button>
-                            </Link>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </section>
+                <Pagination
+                    postsPerPage={postsPerPage} 
+                    totalPosts={posts.length} 
+                    paginate={paginate} 
+                    currentPage={currentPage}
+                />
             </section>
-            <Pagination
-                postsPerPage={postsPerPage} 
-                totalPosts={posts.length} 
-                paginate={paginate} 
-                currentPage={currentPage}
-            />
+            <section className='posts-best'>
+                    <h1>Most viewed posts</h1>
+                    {mostViewed.map(post => {
+                        return(
+                            <div className='post post-best' key={post.id}>
+                                <div className='post-head best-head'>
+                                    <div className='best-title'>
+                                        <h2>{post.title}</h2>
+                                        {post.edit?
+                                            <p>Edited: {post.edit.slice(0,10)}</p>
+                                        :
+                                            <p>Posted: {post.date.slice(0,10)}</p>
+                                        }   
+                                    </div>
+                                </div>
+                                <p className='post-desc'>{post.desc}</p>
+                                <div className='post-info'>
+                                    {post.user !==undefined ? <p>Posted by: {post.user}</p> : <></>}
+                                    <p>Views: {post.views !==undefined ? post.views : "0"}</p>
+                                </div>
+                                <Link to={`/post/${post.id}`}>
+                                    <button type='button'>Show post</button>
+                                </Link>
+                            </div>
+                        )
+                    })}
+            </section>
         </div>
     )
 }
