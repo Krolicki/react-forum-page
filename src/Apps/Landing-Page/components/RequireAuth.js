79ae73953cbb561c62d8df24add6e27408c4cc8b
unknown <jakub.krolicki.k@gmail.com>
2022-09-16 23:21:11 +0200

    Protected routes works after reload

--- src/Apps/Landing-Page/components/RequireAuth.js
+++ src/Apps/Landing-Page/components/RequireAuth.js
@@ -1,12 +1,23 @@
+import { useCookies } from "react-cookie"
 import { Navigate, Outlet, useLocation } from "react-router-dom"
 import useAuth from "../hooks/useAuth"
 
 export const RequireAuth = () => {
     const {auth} = useAuth()
     const location = useLocation()
+    const [cookies] = useCookies()
+    let user = null
 
+    const readCookiesSure = () => {
+        if(cookies.user)
+            user = cookies.user
+    }
+    if(auth?.user === undefined)
+        readCookiesSure()
+    else
+        user = auth?.user
     return(
-        auth?.user
+        user !== null
           ? <Outlet />
           : <Navigate to="/login" state={{from: location}} replace />
     )
