59a837d5ff683ffb2e7e117f29597ac7a58bb98e
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-09-30 16:30:33 +0200

    LandingPage - Move Firebase config to .env file
    and refresh api key to Firabase

--- src/Apps/Landing-Page/firebase/base.js
+++ src/Apps/Landing-Page/firebase/base.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth } from "firebase/auth"
 
 const base_app = initializeApp({
-  apiKey: "AIzaSyAmVZkuTzJ1dvRCJeArrGUkHoAWZOj7pw0",
-  authDomain: "react-workshop-eba4b.firebaseapp.com",
-  projectId: "react-workshop-eba4b",
-  storageBucket: "react-workshop-eba4b.appspot.com",
-  messagingSenderId: "623600921001",
-  appId: "1:623600921001:web:3d3300e5d0a217151b3cb8"
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID
 })
 
 export const base_auth = getAuth(base_app)
