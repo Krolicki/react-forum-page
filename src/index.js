32605cbbf22452f743c215c75deca46eeda32a9c
Jakub Królicki <jakub.krolicki.k@gmail.com>
2022-10-10 22:05:54 +0200

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
-//import {TaksTracker} from './Apps/task-tracker/TaskTracker'
 import {LandingPage} from './Apps'
+//import {Workshop, TaksTracker} from './Apps'
 import { AuthProvider } from './Apps/Landing-Page/context/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
+    //<Workshop />
+    //<TaksTracker />
     <AuthProvider>
       <LandingPage />
     </AuthProvider>
   //</React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
